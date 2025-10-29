6a5477dffae683f7bbee83cac3a28e3e30ed13c0
Your Name <you@example.com>
2025-10-29 16:32:45 -0300

    Ajuste nas efases do texto

--- core/views.py
+++ core/views.py
@@ -4504,16 +4504,16 @@ def detalhes_devedor(request, titulo_id):
     )
     tpl_padrao = (
         "NOTIFICAÇÃO EXTRAJUDICIAL\n\n"
-        "Prezado (a) Cliente %Nome%*, portador do documento de CPF/CNPJ nº: *%CpfCnpjMascarado%*\n\n"
-        "Me chamo %NomeConsultor%, falo em nome da Negociar Cobranças, empresa contratada por %NomeCredor%\n\n"
-        "Comunicamos a existência de valores não pagos junto à empresa %NomeCredor%.\n\n"
+        "Prezado (a) Cliente *%Nome%*, portador do documento de CPF/CNPJ nº: *%CpfCnpjMascarado%*\n\n"
+        "Me chamo *%NomeConsultor%*, falo em nome da Negociar Cobranças, empresa contratada por *%NomeCredor%*\n\n"
+        "Comunicamos a existência de valores não pagos junto à empresa *%NomeCredor%*.\n\n"
         "Solicitamos sua resposta ou comparecimento à loja no prazo de *72 Horas* após o recebimento dessa Notificação, para tratar de assuntos de sua *RESPONSABILIDADE* e *INTERESSE*.\n\n"
         "Entretanto, não sendo e não conhecendo a pessoa acima, pedimos que desconsidere a nossa mensagem.\n\n"
         "Nos colocamos à sua inteira disposição para sanar toda e qualquer dúvida referente à nossa notificação, acesse os canais abaixo:\n\n"
         "Central de Atendimento e Negociações:\n"
-        "WhatsApp: 91991600118\n\n"
+        "WhatsApp: *91991600118*\n\n"
         "Atenciosamente,\n"
-        "%NomeConsultor%\n"
+        "*%NomeConsultor%*\n"
         "Atendimento ao Cliente\n"
         "Negociar Cobranças"
     )
