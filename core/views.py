c879bf4895b72677d4562f0e6c1cbe907b417975
Your Name <you@example.com>
2025-10-30 08:50:32 -0300

    Ajuste

--- core/views.py
+++ core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth import authenticate, login
-# lsdls
+
 from .models import (
     Devedor,
     Empresa,
