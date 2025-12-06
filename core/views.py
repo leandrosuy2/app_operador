cb3692f66de04250e50c561269f74b7c04fbddd1
leandrosuy2 <suyninja@gmail.com>
2025-12-06 12:44:03 -0300

    Ajuste no token da Lemit

--- core/views.py
+++ core/views.py
@@ -4710,7 +4710,7 @@ def consult_api(request):
         data = json.loads(request.body)
         devedores_ids = data.get("devedores", [])
 
-        access_token = "tJ42EaCc79G6RTEDGwM5ZBbJYjWz53WTI33iJSZZ"  # Replace with your actual access token
+        access_token = "ihf97GeOfLQByLgY9YJT3viKoVuIdzhaC0nbjpQE"  # Replace with your actual access token
 
         for devedor_id in devedores_ids:
             devedor = get_object_or_404(Devedor, id=devedor_id)
