1f60823aaa6977d7a99919dfb291e3b549771f7b
leandrosuy2 <suyninja@gmail.com>
2025-12-16 12:22:17 -0300

    ajuste

---
+++ core/migrations/0018_merge_0014_0017.py
@@ -0,0 +1,15 @@
+# Generated manually to merge conflicting migrations
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0014_add_atualizado_por_to_whatsapp_template'),
+        ('core', '0017_alter_whatsapptemplate_empresa'),
+    ]
+
+    operations = [
+    ]
+
